69155e7d7a2cd96b40509f565e1cb90b1580c21e
Lilith0000 <cws985@163.com>
2021-09-30 16:16:53 +0800

    210930 amendment

--- s1_p2.py
+++ s1_p2.py
@@ -1,25 +1,40 @@
+# Introduction:
+# Inputting series of numbers, this program automatically help to count and record things.
+# If input 'done', the program will stop and show results as bellows:
+# 1. total; 2. average; 3. count of numbers; 4. maximum number; 5. minimum number.
+
 # Initial
-Total = 0
-Count = 0
-Average = 0
-Max_num = -99999999
-Min_num = +99999999
+total = 0
+count = 0
+average = None
+max_num = None
+min_num = None
 
 # Main structure
 while True:
     current_num = input('Enter a number: ')
-
     if current_num == 'done':
-        print('You entered ', Count, ' numbers, the total is: ', Total, ', the average is: ', Average)
-        print('The maximum is: ', Max_num, ', the minimum is: ', Min_num)
+        print('You entered ', count, ' numbers, the total is: ', total, ', the average is: ', average)
+        print('The maximum is: ', max_num, ', the minimum is: ', min_num)
         break
     else:
         try:
-            Total = Total + float(current_num)
-            Count = Count + 1
-            Average = Total / Count
-            Max_num = max(Max_num, float(current_num))
-            Min_num = min(Min_num, float(current_num))
+            current_num = float(current_num)
         except ValueError as invalid:
             print('Error type: ', invalid)
+            continue
+        if max_num is None:
+            max_num = current_num
+        else:
+            max_num = max(max_num, current_num)
+        if min_num is None:
+            min_num = current_num
+        else:
+            min_num = min(min_num, current_num)
+        total += current_num
+        count += 1
+        average = total / count
+
+
+
 
